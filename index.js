0230d69783ff2d7770633cabcbc4bd254f5aecd7
Hubert Buczkowski <hubertbuczkowski@Huberts-MacBook-Pro.local>
2020-02-09 21:56:59 +0000

    test 1

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 var express = require('express')
 var app = express()
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-   res.send('hello world')
-})
+   res.send(JSON.stringify({ Hello: 'World' }));
+});
 
-app.listen(3000)
+app.listen(port, function () {
+   console.log(`Example app listening on port !`);
+});
