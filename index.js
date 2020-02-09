8ac8422346e7e2b9d0c0e988519bbf75d42fb928
Hubert Buczkowski <hubertbuczkowski@Huberts-MacBook-Pro.local>
2020-02-09 21:14:25 +0000

    test

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-require('./lib/server')()
+// require('./lib/server')()
+
+const app = require('http').createServer(handler);
+
+app.listen(9000);
+
+function handler(req, res) {
+   var data = '';
+
+
+   console.log(req.url);
+   console.log(url.parse(req.url, true));
+
+   var queryObject = url.parse(req.url, true).query;
+
+   if (req.method == "GET") {
+      req.on('data', function (chunk) {
+         data += chunk;
+      });
+
+      req.on('end', function () {
+         console.log('Received body data:');
+         console.log(data.toString());
+
+      });
+   }
+
+   console.log("Query strings: " + JSON.stringify(queryObject));
+
+   res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
+   res.end();
+}
